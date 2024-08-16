17ea9ba263363d41fb81053eb795047ab7c7af42
lilsis jasper <jasperutilities@fastmail.com>
2024-08-15 23:23:59 -0400

    pronav js so each page has the same pronav

---
+++ assets/pronav.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const proNavHTML = `
+        <div class="profile-pic-box">
+            <div class="profile-pic"></div>
+            <a href="/about">About Jasper</a>
+        </div>
+
+        <div class="nav-box">
+            <h2>Navigation</h2>
+            <a href="/">🏠 Home</a><br>
+            <a href="/diary">📓 Diary</a><br>
+            <a href="/reviews">🎮 Game Reviews</a><br>
+            <a href="/cookbook">🍳 Cookbook</a><br>
+            <a href="/friends">💜 Friends</a><br>
+            <a href="/projects">📂 Cool Projects</a><br>
+        </div>
+    `;
+
+    const proNav = document.querySelector('.pronav');
+    if (proNav) {
+        proNav.innerHTML = proNavHTML;
+    }
+});
