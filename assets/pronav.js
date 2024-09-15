4d36dbc3b2d673bcacd9241e8d879a4fcf0024ce
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2024-09-15 14:12:25 -0500

    Wireframing and adding dead links

--- assets/pronav.js
+++ assets/pronav.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const proNavHTML = `
         <div class="profile-pic-box">
             <div class="profile-pic"></div>
-            <a href="/about">About Jasper</a>
+            <a href="/about.html">About Jasper</a>
         </div>
 
         <div class="nav-box">
             <h2>Navigation</h2>
             <a href="/">🏠 Home</a><br>
             <a href="/diary">📓 Diary</a><br>
-            <a href="/games/home">🎮 Game Reviews</a><br>
+            <a href="/games/home.html">🎮 Game Reviews</a><br>
             <a href="/cookbook">🍳 Cookbook</a><br>
             <a href="/friends">💜 Friends</a><br>
             <a href="/projects">📂 Cool Projects</a><br>
